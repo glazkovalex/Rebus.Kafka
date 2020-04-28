f5b504a9d65757e6244bb116ec5970be43b06bf2
Alexandr <forspam.temp@gmail.com>
2020-04-28 23:39:02 +0300

    Add performance test and update packages

--- Rebus.Kafka.Tests/SimpleTests.cs
+++ Rebus.Kafka.Tests/SimpleTests.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Rebus.Kafka.Tests.Messages;
+using Rebus.Logging;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -17,84 +18,126 @@ namespace Rebus.Kafka.Tests
     //[Collection("ServicesFixture")]
     public class SimpleTests : BaseTestWithServicesFixture
     {
-		[Fact]
-		public async Task SendReceive()
-		{
-			using (var adapter = new BuiltinHandlerActivator())
-			{
-				var amount = 0;
-				Stopwatch sw = Stopwatch.StartNew();
+        [Fact]
+        public async Task SendReceive()
+        {
+            using (var adapter = new BuiltinHandlerActivator())
+            {
+                var amount = 0;
+                Stopwatch sw = Stopwatch.StartNew();
 
-				adapter.Handle<Message>(message =>
-				{
-					amount = amount + message.MessageNumber;
-					Output.WriteLine($"Received : \"{message.MessageNumber}\"");
-					if (message.MessageNumber == MessageCount)
-						Output.WriteLine($"Получено {MessageCount} сообщений за {sw.ElapsedMilliseconds / 1000f:N3}с");
-					return Task.CompletedTask;
-				});
+                adapter.Handle<Message>(message =>
+                {
+                    amount = amount + message.MessageNumber;
+                    Output.WriteLine($"Received : \"{message.MessageNumber}\"");
+                    if (message.MessageNumber == MessageCount)
+                        Output.WriteLine($"Received {MessageCount} messages in {sw.ElapsedMilliseconds / 1000f:N3}s");
+                    return Task.CompletedTask;
+                });
 
-				Configure.With(adapter)
-					.Logging(l => l.Use(new TestOutputLoggerFactory(Output)))
-					.Transport(t => t.UseKafka(Fixture.KafkaEndpoint, nameof(SimpleTests), "temp"))
-					.Routing(r => r.TypeBased().Map<Message>(nameof(SimpleTests)))
-					.Start();
+                Configure.With(adapter)
+                    .Logging(l => l.Use(new TestOutputLoggerFactory(Output)))
+                    .Transport(t => t.UseKafka(Fixture.KafkaEndpoint, nameof(SimpleTests), "temp"))
+                    .Routing(r => r.TypeBased().Map<Message>(nameof(SimpleTests)))
+                    .Start();
 
-				var sendAmount = 0;
-				var messages = Enumerable.Range(1, MessageCount)
-					.Select(i =>
-					{
-						sendAmount = sendAmount + i;
-						return adapter.Bus.Send(new Message { MessageNumber = i });
-					}).ToArray();
+                var sendAmount = 0;
+                var messages = Enumerable.Range(1, MessageCount)
+                    .Select(i =>
+                    {
+                        sendAmount = sendAmount + i;
+                        return adapter.Bus.Send(new Message { MessageNumber = i });
+                    }).ToArray();
 
-				Task.WaitAll(messages);
-				await Task.Delay(10000);
+                Task.WaitAll(messages);
+                await Task.Delay(10000);
 
-				Assert.Equal(amount, sendAmount);
-			}
-		}
+                Assert.Equal(amount, sendAmount);
+            }
+        }
 
-		[Fact]
-		public async Task PublishSubscribe()
-		{
-			IContainer container;
-			var builder = new ContainerBuilder();
-			builder.RegisterInstance(Output).As<ITestOutputHelper>().SingleInstance();
-			builder.RegisterType<MessageHandler>().As(typeof(IHandleMessages<>).MakeGenericType(typeof(Message)));
-			builder.RegisterRebus((configurer, context) => configurer
-				.Logging(l => l.Use(new TestOutputLoggerFactory(Output)))
-				.Transport(t => t.UseKafka(Fixture.KafkaEndpoint, nameof(SimpleTests)))
-				.Options(o => o.SetMaxParallelism(5))
-			);
+        [Fact]
+        public async Task PublishSubscribe()
+        {
+            IContainer container;
+            var builder = new ContainerBuilder();
+            builder.RegisterInstance(Output).As<ITestOutputHelper>().SingleInstance();
+            builder.RegisterType<MessageHandler>().As(typeof(IHandleMessages<>).MakeGenericType(typeof(Message)));
+            builder.RegisterRebus((configurer, context) => configurer
+                .Logging(l => l.Use(new TestOutputLoggerFactory(Output)))
+                .Transport(t => t.UseKafka(Fixture.KafkaEndpoint, nameof(SimpleTests)))
+                .Options(o => o.SetMaxParallelism(5))
+            );
 
-			using (container = builder.Build())
-			using (IBus bus = container.Resolve<IBus>())
-			{
-				await bus.Subscribe<Message>();
-				var sendAmount = 0;
-				var messages = Enumerable.Range(1, MessageCount)
-					.Select(i =>
-				   {
-					   sendAmount = sendAmount + i;
-					   return bus.Publish(new Message { MessageNumber = i });
-				   }).ToArray();
+            using (container = builder.Build())
+            using (IBus bus = container.Resolve<IBus>())
+            {
+                await bus.Subscribe<Message>();
+                var sendAmount = 0;
+                var messages = Enumerable.Range(1, MessageCount)
+                    .Select(i =>
+                   {
+                       sendAmount = sendAmount + i;
+                       return bus.Publish(new Message { MessageNumber = i });
+                   }).ToArray();
 
-				Task.WaitAll(messages);
-				await Task.Delay(10000);
+                Task.WaitAll(messages);
+                await Task.Delay(10000);
 
-				Assert.Equal(Amount, sendAmount);
-			}
-		}
+                Assert.Equal(Amount, sendAmount);
+            }
+        }
 
-		#region Settings
+        [Fact]
+        public async Task Performance()
+        {
+            int perfomanceCount = 10000;
+            using (var adapter = new BuiltinHandlerActivator())
+            {
+                Stopwatch swHandle = null;
 
-		internal static int Amount;
-		const int MessageCount = 10;
+                adapter.Handle<Message>(message =>
+                {
+                    if (swHandle == null)
+                    {
+                        swHandle = Stopwatch.StartNew();
+                    }
+                    if (message.MessageNumber == perfomanceCount)
+                    {
+                        swHandle.Stop();
+                        Output.WriteLine($"Received {perfomanceCount} messages in {swHandle.ElapsedMilliseconds / 1000f:N3}s");
+                    }
+                    return Task.CompletedTask;
+                });
+
+                Configure.With(adapter)
+                    .Logging(l => l.Use(new TestOutputLoggerFactory(Output) { MinLevel = LogLevel.Warn }))
+                    .Transport(t => t.UseKafka(Fixture.KafkaEndpoint, nameof(SimpleTests), "temp"))
+                    .Routing(r => r.TypeBased().Map<Message>(nameof(SimpleTests)))
+                    .Start();
+
+                var messages = Enumerable.Range(1, perfomanceCount)
+                    .Select(i => adapter.Bus.Send(new Message { MessageNumber = i })).ToArray();
+                Stopwatch swSend = Stopwatch.StartNew();
+
+                Task.WaitAll(messages);
+
+                swSend.Stop();
+                Output.WriteLine($"Send {perfomanceCount} messages in {swSend.ElapsedMilliseconds / 1000f:N3}s.");
+
+                await Task.Delay(10000);
+                Assert.True(swSend.ElapsedMilliseconds < 10000);
+            }
+        }
+
+        #region Settings
+
+        internal static int Amount;
+        const int MessageCount = 10;
 
         /// <summary>Creates new instance <see cref="SimpleTests"/>.</summary>
         public SimpleTests(ServicesFixture fixture, ITestOutputHelper output) : base(fixture, output) { }
 
-		#endregion
-	}
+        #endregion
+    }
 }
