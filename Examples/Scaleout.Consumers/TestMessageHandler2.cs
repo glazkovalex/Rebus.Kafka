8195b1fd574f529fa7863b72f7214988c441712b
Alexandr <forspam.temp@gmail.com>
2023-12-10 19:43:29 +0300

    Scaleout via two test messages

---
+++ Examples/Scaleout.Consumers/TestMessageHandler2.cs
@@ -0,0 +1,28 @@
+﻿using Rebus.Bus;
+using Rebus.Handlers;
+using Scaleout.Messages;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scaleout.Consumers
+{
+	/// <inheritdoc />
+	public class TestMessageHandler2 : IHandleMessages<TestMessage2>
+	{
+		/// <inheritdoc />
+		public async Task Handle(TestMessage2 evnt)
+		{
+			await Task.Delay(1000);
+			Console.WriteLine($"Received Message2 : \"{evnt.MessageNumber}\" in thread {Thread.CurrentThread.ManagedThreadId}. Send Confirmation...");
+			await _bus.Publish(new Confirmation { MessageNumber = evnt.MessageNumber });
+		}
+
+		public static int Amount;
+		private readonly IBus _bus;
+		public TestMessageHandler2(IBus bus)
+		{
+			_bus = bus;
+		}
+	}
+}
---
+++ Examples/Scaleout.Messages/TestMessage2.cs
@@ -0,0 +1,8 @@
+﻿namespace Scaleout.Messages
+{
+	/// <summary>Message class used for testing</summary>
+	public class TestMessage2
+	{
+		public int MessageNumber { get; set; }
+	}
+}
